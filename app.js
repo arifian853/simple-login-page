1a647d5a2f285f42b60c57d44c77c0c4b68f4857
Arifian Saputra <arifiansaputra43@gmail.com>
2021-09-11 13:12:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 100,
+        "value": 110,
         "density": {
           "enable": true,
           "value_area": 800
@@ -26,7 +26,7 @@ particlesJS('particles-js',
         "value": "#E6EBE0"
       },
       "shape": {
-        "type": "circle",
+        "type": "triangle",
         "stroke": {
           "width": 0,
           "color": "#000000"
